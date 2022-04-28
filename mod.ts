06533f1e32f63e90c1c61e74cd7b340cfcdf9001
错别字 <2274901206@qq.com>
2022-04-29 00:08:43 +0800

    init

--- mod.ts
+++ mod.ts
@@ -1,5 +1,26 @@
-// const { args: [name] } = Deno
+const { args: [name] } = Deno;
+import { ensureDir } from "https://deno.land/std/fs/ensure_dir.ts";
 
-export default () => {
-    console.log('hello')
+const compoContent: string = `import React from 'react';
+import PropTypes from 'prop-types';
+
+${name}.propTypes = {
+    
+};
+function ${name}(props) {
+    return (
+        <div className="${name.toLowerCase()}">
+        </div>
+    );
 }
+export default ${name};
+`;
+
+
+const compoPath: string = "./app/components/" + name + "/";
+
+ensureDir(compoPath)
+  .then(() => {
+    Deno.writeTextFile(compoPath + "index.js", compoContent);
+    console.info("Done!");
+});
